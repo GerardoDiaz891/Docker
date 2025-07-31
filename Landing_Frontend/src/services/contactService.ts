aef3719432e422d9764905012c7b856c937d8cd9
WilliansAS <133081713+WilliansAS@users.noreply.github.com>
2025-07-31 02:38:26 -0500

    Update contactService.ts

--- Landing_Frontend/src/services/contactService.ts
+++ Landing_Frontend/src/services/contactService.ts
@@ -1,21 +1,21 @@
 import { genericRequest, genericRequestAuth } from '@/utils/genericRequest'
 
 export const getContacts = async () => {
-  const response = genericRequestAuth('/contacts', 'GET')
+  const response = await genericRequestAuth('/contacts', 'GET')
   return response
 }
 
 export const createContact = async (data: any) => {
-  const response = genericRequest('/contacts', 'POST', data)
+  const response = await genericRequest('/contacts', 'POST', data)
   return response
 }
 
 export const updateContact = async (id: number, data: any) => {
-  const response = genericRequestAuth(`/contacts/${id}`, 'PUT', data)
+  const response = await genericRequestAuth(`/contacts/${id}`, 'PUT', data)
   return response
 }
 
 export const deleteContact = async (id: number) => {
-  const response = genericRequestAuth(`/contacts/${id}`, 'DELETE')
+  const response = await genericRequestAuth(`/contacts/${id}`, 'DELETE')
   return response
 }
