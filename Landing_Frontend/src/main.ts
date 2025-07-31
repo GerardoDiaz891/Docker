d168e0813a153146fa4521f1dca33f75aa7313dc
WilliansAS <133081713+WilliansAS@users.noreply.github.com>
2025-07-31 14:09:34 -0500

    Update main.ts

--- Landing_Frontend/src/main.ts
+++ Landing_Frontend/src/main.ts
@@ -6,15 +6,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import { install as Vue3Recaptcha } from 'vue3-recaptcha-v2'
-
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
-app.use(Vue3Recaptcha, {
-  sitekey: '6LcUZmsrAAAAAED4P8m9xlzAJ7Z7G-TRlqy16rc4',
-})
 
 app.mount('#app')
